a3cfab44aee4ce860c371cd4f4daeb1b1a49d8ca
kdg0209 <s_cova_0418@naver.com>
2023-07-27 21:40:19 +0900

    feat: redis 분산락 적용

--- src/main/java/com/gift/kakao/domain/receivedgiftbox/dto/ReceivedGiftBoxCreateRequest.java
+++ src/main/java/com/gift/kakao/domain/receivedgiftbox/dto/ReceivedGiftBoxCreateRequest.java
@@ -1,10 +1,12 @@
 package com.gift.kakao.domain.receivedgiftbox.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
 @Getter
+@NoArgsConstructor
 public class ReceivedGiftBoxCreateRequest {
 
     @NotBlank
@@ -12,4 +14,9 @@ public class ReceivedGiftBoxCreateRequest {
 
     @NotBlank
     private String giftSerialCode; // redis에서 저장중인 serialCode 형식에서 ex) 2307220010FA9HHIEX:2307220010J0NPV6IU
+
+    public ReceivedGiftBoxCreateRequest(String memberId, String giftSerialCode) {
+        this.memberId = memberId;
+        this.giftSerialCode = giftSerialCode;
+    }
 }
